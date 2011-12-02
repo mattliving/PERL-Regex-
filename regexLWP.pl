07df61243a72b18fb0b58cebd04cd95029ee4d9a
Matthew Livingston <matt.l.livingston@gmail.com>
2011-12-02 17:26:41 +0000

    regexLWP

---
+++ regexLWP.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -W
+use LWP::UserAgent;
+use HTTP::Request::Common qw(POST);
+use HTTP::Request::Common qw(GET);
+
+# Define new user agent
+$ua = LWP::UserAgent->new;
+
+my $tempFile = "tempFile.txt";
+$htmlTag = qr/(\<\/?[^\>]+\>)/;
+$num_args = $#ARGV + 1;
+if ($num_args < 2 || $num_args > 5) {
+	print "error: enter climbing grades in the form '5A|5B' followed by one - four url(s)";
+}
+else {
+	@urls = ("", "", "", "");
+	$skill = $ARGV[0];
+	foreach $argnum (1 .. $#ARGV) {
+		$urls[$argnum-1] = $ARGV[$argnum];
+	}
+}	
+
+
+# Request object
+$req = POST $urls[0], [];
+# Make the request
+$res = $ua->request($req, $tempFile);
+# Check response
+open FILE,($tempFile) or die "error opening temp file: $!";
+open $outFile, '>', 'de pakhuys climbing routes.txt' or die "error writing to file: $!";
+if ($res->is_success) {
+	while (<FILE>) {
+		if (m/((?:[A-Z|a-z]\d+)|(?:\d+\.))(.*)($skill)&nbsp;(?:$htmlTag*)\s*(.*$)/i) {
+			print $1, $2, $3, " - " , $5, "\n";
+			print $outFile $1, $2, $3, " - " , $5, "\n\n";
+		}
+	}
+} else {
+    print $res->status_line . "\n";
+}
+close FILE;
+close $outFile;
